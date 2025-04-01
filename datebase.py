36883afdeb54e699ff66624119c255978f07cc93
신관규 <soundbar91@gmail.com>
2025-04-01 15:56:15 +0900

    fix databaes.py

--- datebase.py
+++ datebase.py
@@ -1,14 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 import config
 
+Base = declarative_base()
 db = config.DATABASE_CONFIG
-DB_URL = f"mysql+pymysql://{db['USERNAME']}:{db['PASSWORD']}@{db['HOST']}:{db['PORT']}/{db['DBNAME']}"
+DB_URL = f"mysql+pymysql://{db['USERNAME']}:{db['PASSWORD']}@{db['HOST']}:{db['PORT']}/{db['DBNAME']}?charset=utf8"
 
 class engineconn:
 
     def __init__(self):
-        self.engine = create_engine(DB_URL, pool_recycle = 500)
+        self.engine = create_engine(DB_URL, pool_recycle=500)
     
     def sessionmaker(self):
         Session = sessionmaker(bind=self.engine)
@@ -16,5 +18,13 @@ class engineconn:
         return session
     
     def connecton(self):
-        conn = self.engine.connect()
-        return conn
+        return self.engine.connect()
+
+def get_db():
+    db = engineconn().sessionmaker()
+    try:
+        yield db
+    finally:
+        db.close()
+
+engine = engineconn().engine
