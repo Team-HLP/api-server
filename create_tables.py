8903b586a1762bb567fbd259c0b52dec74c0d4f4
신관규 <soundbar91@gmail.com>
2025-04-01 14:22:59 +0900

    refactor create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
 from models import Base
 import config
+import datebase
 
-db = config.DATABASE_CONFIG
-DB_URL = f"mysql+pymysql://{db['USERNAME']}:{db['PASSWORD']}@{db['HOST']}:{db['PORT']}/{db['DBNAME']}"
-
-engine = create_engine(DB_URL)
+engine = create_engine(datebase.DB_URL)
 
 Base.metadata.create_all(bind=engine)
