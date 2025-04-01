50a8944c2520bb1224aa3fe35288be83ff1fb3d7
신관규 <soundbar91@gmail.com>
2025-04-02 01:12:41 +0900

    feat: 비밀번호 해쉬 함수 추가

--- user/router.py
+++ user/router.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from datebase import get_db
 from user.models import Users
 from user.schemas import *
+from user.auth import *
 
 router = APIRouter()
 
@@ -12,9 +13,10 @@ def register_user(user_create: UserCreateRequest, db: Session = Depends(get_db))
     if exist_user:
         raise HTTPException(status_code=409, detail="이미 존재하는 사용자 ID입니다.")
     
+    hashed_pw = hash_password(user_create.password)
     new_user = Users(
         login_id=user_create.login_id,
-        password=user_create.password,
+        password=hashed_pw,
         username=user_create.username,
         sex=user_create.sex
     )
