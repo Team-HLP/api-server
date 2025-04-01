0731a16e650d26f0737410bef18747b161aecf50
신관규 <soundbar91@gmail.com>
2025-04-01 14:30:12 +0900

    add models.py

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, TEXT, INT, BIGINT, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.mysql import MEDIUMTEXT
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(BIGINT, nullable=False, autoincrement=True, primary_key=True, comment="유저 고유 id")
+    login_id = Column(String(20), nullable=False, unique=True, comment="유저 로그인 id")
+    password = Column(MEDIUMTEXT, nullable=False, comment="`유저 비밀번호")
+    username = Column(String(50), nullable=False, comment="유저 이름")
+    sex = Column(Boolean, nullable=False, comment="성별")
