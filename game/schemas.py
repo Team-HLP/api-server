68a3fc3d001873d12443a3e3029f02a60394bdc5
신관규 <soundbar91@gmail.com>
2025-04-02 11:48:46 +0900

    feat: 게임 조회 API 추가

--- game/schemas.py
+++ game/schemas.py
@@ -6,7 +6,7 @@ class GameCreateRequest(BaseModel):
     play_time_seconds: int
 
 # Response
-class GameCreateResponse(BaseModel):
+class GameResponse(BaseModel):
     id: int
     user_id: int
     meteorite_broken_count: int
