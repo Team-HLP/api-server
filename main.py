26accf3ec9d12b9dd5a1890301a786481c64cd86
신관규 <soundbar91@gmail.com>
2025-04-02 11:02:46 +0900

    feat: Game 라우터 추가

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
+from datebase import Base, engine
 from user.router import router as user_router
 from user.models import User
+from game.router import router as game_router
 from game.models import Game
-from datebase import Base, engine
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(game_router, prefix="/game", tags=["game"])
 
 Base.metadata.create_all(bind=engine)
