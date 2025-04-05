9f62c514ad07b1ae58e36c4cf9fac304dfd9e292
신관규 <soundbar91@gmail.com>
2025-04-05 20:39:34 +0900

    feat: 게임 컬럼 추가

--- game/router.py
+++ game/router.py
@@ -17,7 +17,9 @@ def save_game(
     new_game = Game(
         user_id=current_user.id,
         meteorite_broken_count=game_create.meteorite_broken_count,
-        play_time_seconds=game_create.play_time_seconds
+        play_time_seconds=game_create.play_time_seconds,
+        blink_eye_count=game_create.blink_eye_count,
+        avg_pupil_size=game_create.avg_pupil_size
     )
 
     db.add(new_game)
@@ -28,7 +30,9 @@ def save_game(
         id=new_game.id,
         user_id=current_user.id,
         meteorite_broken_count=new_game.meteorite_broken_count,
-        play_time_seconds=new_game.play_time_seconds
+        play_time_seconds=new_game.play_time_seconds,
+        blink_eye_count=game_create.blink_eye_count,
+        avg_pupil_size=game_create.avg_pupil_size
     )
 
 @router.get("", response_model=list[GameResponse], status_code=200)
@@ -38,7 +42,7 @@ def get_games(
 ):
     return db.query(Game).filter(Game.user_id == current_user.id).all()
 
-@router.get("/{game_id}", response_model=GameResponse, status_code=200)
+@router.get("/{gameId}", response_model=GameResponse, status_code=200)
 def get_game(
     game_id: int,
     current_user: User = Depends(get_current_user), 
--- game/schemas.py
+++ game/schemas.py
@@ -1,13 +1,19 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 # Request
 class GameCreateRequest(BaseModel):
     meteorite_broken_count: int
     play_time_seconds: int
+    blink_eye_count: int
+    avg_pupil_size: float
 
 # Response
 class GameResponse(BaseModel):
-    id: int
+    game_id: int
     user_id: int
     meteorite_broken_count: int
     play_time_seconds: int
+    blink_eye_count: int
+    avg_pupil_size: float
+    created_at: datetime
