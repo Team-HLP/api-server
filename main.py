f45fc293dfc1958bf3dfe81a0484a2f6e4cf7f17
신관규 <soundbar91@gmail.com>
2025-04-05 19:07:44 +0900

    feat: 아이트래킹 테스트 api 추가

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from user.router import router as user_router
 from user.models import User
 from game.router import router as game_router
 from game.models import Game
+from gaze.router import router as gaze_router
+from gaze.models import GazeData
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/user", tags=["user"])
 app.include_router(game_router, prefix="/game", tags=["game"])
+app.include_router(gaze_router, prefix="/gaze", tags=["gaze"])
 
 Base.metadata.create_all(bind=engine)
